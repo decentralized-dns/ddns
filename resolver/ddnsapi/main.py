c6225d00328ec932aeaa290c25aa3e324aefe99c
Rui Chen <rui@chenrui.dev>
2023-02-25 18:46:43 +0100

    add default homepage and flyctl configs
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- resolver/ddnsapi/main.py
+++ resolver/ddnsapi/main.py
@@ -1,10 +1,12 @@
-from ddnsapi.routes.ping import router
+from ddnsapi.routes.home import router as home_router
+from ddnsapi.routes.ping import router as ping_router
 from fastapi import FastAPI
 
 
 def get_application() -> FastAPI:
     app = FastAPI()
-    app.include_router(router)
+    app.include_router(home_router)
+    app.include_router(ping_router)
     return app
 
 
