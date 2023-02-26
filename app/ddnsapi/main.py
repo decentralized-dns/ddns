33d07239c8b62f235a7412e97a93cbe620211e4b
Rui Chen <rui@chenrui.dev>
2023-02-26 16:58:41 +0100

    run `black` fmt
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- app/ddnsapi/main.py
+++ app/ddnsapi/main.py
@@ -1,4 +1,3 @@
-
 from ddnsapi.routes.ping import router as ping_router
 from fastapi import FastAPI, Form, Request
 from fastapi.middleware.cors import CORSMiddleware
@@ -28,18 +27,19 @@ app = get_application()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/', response_class=HTMLResponse)
+
+@app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get('/{page}', response_class=HTMLResponse)
+@app.get("/{page}", response_class=HTMLResponse)
 async def display_page(request: Request, page: str):
     return templates.TemplateResponse(f"{page}.html", {"request": request})
 
 
 @app.post("/search", response_class=HTMLResponse)
-async def search(request: Request, domain_name: str = Form("domain_name")): #,
+async def search(request: Request, domain_name: str = Form("domain_name")):  # ,
     # domain_name= request.args['domain_name']
     # print("-" * 40)
     # print("domain_name", domain_name)
@@ -65,69 +65,90 @@ async def search(request: Request, domain_name: str = Form("domain_name")): #,
     address = 1
 
     # Your domain search logic here
-    return templates.TemplateResponse("search.html",
-                                      {"request": request, "domain_name": domain_name, "domain_info": domain_info,
-                                       "parent": parent, "registrant": registrant, "controller": controller,
-                                       "expiration_date": expiration_date, "resolver": resolver, "email": email,
-                                       "url": url, "avatar": avatar, "description": description, "notice": notice,
-                                       "keywords": keywords, "dicord": dicord, "github": github, "reddit": reddit,
-                                       "telegram": telegram, "address":address})
+    return templates.TemplateResponse(
+        "search.html",
+        {
+            "request": request,
+            "domain_name": domain_name,
+            "domain_info": domain_info,
+            "parent": parent,
+            "registrant": registrant,
+            "controller": controller,
+            "expiration_date": expiration_date,
+            "resolver": resolver,
+            "email": email,
+            "url": url,
+            "avatar": avatar,
+            "description": description,
+            "notice": notice,
+            "keywords": keywords,
+            "dicord": dicord,
+            "github": github,
+            "reddit": reddit,
+            "telegram": telegram,
+            "address": address,
+        },
+    )
 
 
-
-@app.get('/addressinfo', response_class=HTMLResponse)
+@app.get("/addressinfo", response_class=HTMLResponse)
 async def get_address_info(request: Request, account_address: str = Form(...)):
     default_domain = get_default_domain(account_address)
     asso_domains = get_asso_domains(account_address)
     return templates.TemplateResponse(
         "search.html",
-        {"request": request,
-         "account_address": account_address,
-         "default_domain": default_domain,
-         "asso_domains": asso_domains}
+        {
+            "request": request,
+            "account_address": account_address,
+            "default_domain": default_domain,
+            "asso_domains": asso_domains,
+        },
     )
+
+
 def get_default_domain(account_address):
-    return 'TestDomain'
+    return "TestDomain"
+
 
 def get_asso_domains(account_address):
-    return ('AABB','CCDD')
+    return ("AABB", "CCDD")
 
 
-
-@app.get('/register', response_class=HTMLResponse)
-async def register_status(request: Request, register_name: str = Form(...),
-                          register_duration: str = Form(...)):
+@app.get("/register", response_class=HTMLResponse)
+async def register_status(
+    request: Request, register_name: str = Form(...), register_duration: str = Form(...)
+):
     reg_status = register_name(register_name, register_duration)
     return {"reg_status": reg_status}
+
 
 def register_name(register_name, register_duration):
     return "Success"
 
 
-@app.get('/extend', response_class=HTMLResponse)
-async def extend_status(request: Request, extend_name: str = Form(...),
-                          extend_duration: str = Form(...)):
+@app.get("/extend", response_class=HTMLResponse)
+async def extend_status(
+    request: Request, extend_name: str = Form(...), extend_duration: str = Form(...)
+):
     extend_status = extend_name(extend_name, extend_duration)
     return templates.TemplateResponse(
-        "manage.html",
-        {"request": request,
-         "extend_status": extend_status}
+        "manage.html", {"request": request, "extend_status": extend_status}
     )
+
 
 def extend_name(extend_name, extend_duration):
     return "Success"
 
 
-
-@app.get('/renew', response_class=HTMLResponse)
-async def renew_status(request: Request, renew_name: str = Form(...),
-                          renew_duration: str = Form(...)):
+@app.get("/renew", response_class=HTMLResponse)
+async def renew_status(
+    request: Request, renew_name: str = Form(...), renew_duration: str = Form(...)
+):
     renew_status = renew_name(renew_name, renew_duration)
     return templates.TemplateResponse(
-        "manage.html",
-        {"request": request,
-         "renew_status": renew_status}
+        "manage.html", {"request": request, "renew_status": renew_status}
     )
+
 
 def renew_name(renew_name, renew_duration):
     return "Success"
@@ -142,11 +163,15 @@ async def connect_pera(request: Request):
             "name": "Pera",
             "type": "WalletConnect",
             "url": "https://app.pera.finance/",
-            "icon": "https://app.pera.finance/favicon.ico"
-        }
+            "icon": "https://app.pera.finance/favicon.ico",
+        },
     }
     headers = {"Content-Type": "application/json"}
-    response = requests.post("https://api.algosigner.io/v2/wallets", data=json.dumps(payload), headers=headers)
+    response = requests.post(
+        "https://api.algosigner.io/v2/wallets",
+        data=json.dumps(payload),
+        headers=headers,
+    )
 
     # Check if the connection was successful
     if response.status_code == 200:
