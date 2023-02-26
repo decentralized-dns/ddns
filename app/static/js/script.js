e464f991cb9b841331d455a4cd57f61e8809c9d0
Rui Chen <rui@chenrui.dev>
2023-02-26 14:22:20 +0100

    fmt static files
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,31 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  "use strict";
 
-	'use strict';
+  /* Animations Home Page*/
+  function onScrollInit(items, trigger) {
+    items.each(function () {
+      var osElement = $(this),
+        osAnimationClass = osElement.attr("data-animation"),
+        osAnimationDelay = osElement.attr("data-animation-delay");
 
-	/* Animations Home Page*/
-	function onScrollInit( items, trigger ) {
-	    items.each( function() {
-	    var osElement = $(this),
-	    osAnimationClass = osElement.attr('data-animation'),
-	    osAnimationDelay = osElement.attr('data-animation-delay');
+      osElement.css({
+        "-webkit-animation-delay": osAnimationDelay,
+        "-moz-animation-delay": osAnimationDelay,
+        "animation-delay": osAnimationDelay,
+      });
 
-	    osElement.css({
-	    '-webkit-animation-delay':  osAnimationDelay,
-	    '-moz-animation-delay':     osAnimationDelay,
-	    'animation-delay':          osAnimationDelay
-	    });
+      var osTrigger = trigger ? trigger : osElement;
 
-	    var osTrigger = ( trigger ) ? trigger : osElement;
-
-	    osTrigger.waypoint(function() {
-	    osElement.addClass('animated').addClass(osAnimationClass);
-	    },{
-	        triggerOnce: true,
-	        offset: '90%'
-	    });
-	    });
-	    }
-		onScrollInit( $('.scroll-animation') );
-	    onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
-
+      osTrigger.waypoint(
+        function () {
+          osElement.addClass("animated").addClass(osAnimationClass);
+        },
+        {
+          triggerOnce: true,
+          offset: "90%",
+        }
+      );
+    });
+  }
+  onScrollInit($(".scroll-animation"));
+  onScrollInit($(".staggered-animation"), $(".staggered-animation-container"));
 });
