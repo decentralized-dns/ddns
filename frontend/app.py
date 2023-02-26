479c67c540ad92a998fb9552c5d72e47184dedfd
Lanming Ma <rachelma1401@gmail.com>
2023-02-25 22:22:19 -0500

    updated frontend v2
    change the page layout. added connect wallet.

--- frontend/app.py
+++ frontend/app.py
@@ -1,222 +1,120 @@
-from flask import Flask, request, jsonify, render_template, request, redirect, url_for
-from algosdk.v2client import algod
-from algosdk import mnemonic, transaction, encoding, name
-# from algosdk.future import transaction as futuretxn
-from pyteal import *
+from fastapi import FastAPI, Request, Form, Response, Query
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = Flask(__name__)
-algod_client = algod.AlgodClient('<algod-token>', '<algod-address>')  # Replace with your own Algod credentials
-#
-# mnemonic_secret = '<your-mnemonic>'
-# mnemonic_phrase = mnemonic.from_private_key(encoding.decode_private_key(mnemonic_secret))
-# account_address = mnemonic.to_public_key(mnemonic_phrase)
-account_address = '1'
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+# from algosdk.v2client import algod
+# from algosdk import mnemonic, transaction, encoding, name
+# from pyteal import *
 
 
-@app.route('/<page>')
-def display_page(page):
-    return render_template(f'{page}.html')
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.route('/search', methods=['GET'])
-def search_domain():
-    domain_name = request.args.get('domain_name')
-
-    domain_info = get_domain_info(domain_name)
-    text_record = get_text_record(domain_name)
-    domain_owner = get_domain_owner(domain_name)
-
-    return render_template('search.html', domain_name=domain_name,
-                           domain_info=domain_info, text_record=text_record,
-                           domain_owner=domain_owner)
+@app.get('/', response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-def get_domain_info(domain_name):
-    # try:
-    #     local_state = algod_client.account_info(account_address)['apps-local-state'][0]['key-value']
-    # except (KeyError, IndexError):
-    #     return jsonify({'error': 'No names registered'}), 404
-    # names = []
-    # for item in local_state:
-    #     if item['key'] == f'info:{domain_name}'.encode('utf-8'):
-    #         return {'ip': item['value']['bytes'].decode('utf-8')}
-    return 'TestIP'
+@app.get('/{page}', response_class=HTMLResponse)
+async def display_page(request: Request, page: str):
+    return templates.TemplateResponse(f"{page}.html", {"request": request})
 
 
-def get_text_record(domain_name):
-    # if not domain_name:
-    #     return jsonify({'error': 'Missing required parameter(s)'}), 400
-    # try:
-    #     local_state = algod_client.account_info(account_address)['apps-local-state'][0]['key-value']
-    # except (KeyError, IndexError):
-    #     return jsonify({'error': 'No names registered'}), 404
-    # for item in local_state:
-    #     if item['key'] == f'text:{domain_name}'.encode('utf-8'):
-    #         text_record = item['value']['bytes']
-    #         return jsonify({'text_record': text_record.decode('utf-8')})
-    return 'TestTextRecord'
+@app.post("/search", response_class=HTMLResponse)
+async def search(request: Request, domain_name: str = Form("domain_name")):
+
+    request = 1
+    domain_info = 1
+    parent = 1
+    registrant = 1
+    controller = 1
+    expiration_date = 1
+    resolver = 1
+    email = 1
+    url = 1
+    avatar = 1
+    description = 1
+    notice = 1
+    keywords = 1
+    dicord = 1
+    github = 1
+    reddit = 1
+    telegram = 1
+
+    # Your domain search logic here
+    return templates.TemplateResponse("search.html",
+                                      {"request": request, "domain_name": domain_name, "domain_info": domain_info,
+                                       "parent": parent, "registrant": registrant, "controller": controller,
+                                       "expiration_date": expiration_date, "resolver": resolver, "email": email,
+                                       "url": url, "avatar": avatar, "description": description, "notice": notice,
+                                       "keywords": keywords, "dicord": dicord, "github": github, "reddit": reddit,
+                                       "telegram": telegram})
 
 
-def get_domain_owner(domain_name):
-    # try:
-    #     local_state = algod_client.account_info(account_address)['apps-local-state'][0]['key-value']
-    # except (KeyError, IndexError):
-    #     return {'error': 'No names registered'}
-    # for item in local_state:
-    #     if item['key'] == f'owner:{domain_name}'.encode('utf-8'):
-    #         return {'owner': item['value']['bytes'].decode('utf-8')}
-    return 'Test Domain owner'
 
-
-@app.route('/addressinfo', methods=['GET'])
-# def get_address_info():
-#   account_address = request.args.get('account_address')
-#   default_domain = get_default_domain(account_address)
-#   asso_domains = get_asso_domains(account_address)
-#   return default_domain
-# #
-#
-def get_address_info():
-  account_address = request.args.get('account_address')
-  default_domain = get_default_domain(account_address)
-  asso_domains = get_asso_domains(account_address)
-  return render_template('search.html', account_address=account_address,
-                         asso_domains=asso_domains,
-                         default_domain=default_domain)
-
+@app.get('/addressinfo', response_class=HTMLResponse)
+async def get_address_info(request: Request, account_address: str = Form(...)):
+    default_domain = get_default_domain(account_address)
+    asso_domains = get_asso_domains(account_address)
+    return templates.TemplateResponse(
+        "search.html",
+        {"request": request,
+         "account_address": account_address,
+         "default_domain": default_domain,
+         "asso_domains": asso_domains}
+    )
 def get_default_domain(account_address):
-    # try:
-    #     local_state = algod_client.account_info(account_address)['apps-local-state'][0]['key-value']
-    # except (KeyError, IndexError):
-    #     return jsonify({'error': 'No default domain registered'}), 404
-    # for item in local_state:
-    #     if item['key'] == b'default_domain':
-    #         default_domain = item['value']['bytes'].decode('utf-8')
-    #     return jsonify({'default_domain': default_domain})
     return 'TestDomain'
 
-
 def get_asso_domains(account_address):
-    # try:
-    #     local_state = algod_client.account_info(account_address)['apps-local-state'][0]['key-value']
-    # except (KeyError, IndexError):
-    #     return jsonify({'error': 'No default domain registered'}), 404
-    # for item in local_state:
-    #     if item['key'] == b'default_domain':
-    #         default_domain = item['value']['bytes'].decode('utf-8')
     return ('AABB','CCDD')
 
 
-@app.route('/register', methods=['GET'])
-def register_status():
-    register_name = request.json.get('register_name')
-    register_address = request.json.get('register_address')
-    register_duration = request.json.get('register_duration')
-    reg_status = register_name(register_name, register_address, register_duration)
-    return render_template('manage.html', reg_status=reg_status)
+
+@app.get('/register', response_class=HTMLResponse)
+async def register_status(request: Request, register_name: str = Form(...),
+                          register_duration: str = Form(...)):
+    reg_status = register_name(register_name, register_duration)
+    return {"reg_status": reg_status}
+
+def register_name(register_name, register_duration):
+    return "Success"
 
 
-def register_name(register_name, register_address, register_duration):
-    if not register_name or not register_address or not register_duration:
-        return jsonify({'error': 'Missing required parameter(s)'}), 400
-    # if not encoding.is_valid_address(register_address):
-    #     return jsonify({'error': 'Invalid Algorand address'}), 400
-    #
-    # program = Seq([
-    #     App.localPut(Int(0), Bytes("owner"), Txn.sender()),
-    #     App.localPut(Int(0), Bytes("address"), Bytes(address)),
-    #     App.localPut(Int(0), Bytes("duration"), Int(duration)),
-    #     Return(Int(1))
-    # ])
-    # teal_code = program.teal()
-    # compiled = algod_client.compile(teal_code)
-    # txn = transaction.Transaction(
-    #     sender=account_address,
-    #     fee=algod_client.suggested_params()['fee'],
-    #     first=algod_client.suggested_params()['last'],
-    #     last=algod_client.suggested_params()['last'] + 1000,
-    #     gh=algod_client.suggested_params()['genesis_hash'],
-    #     **compiled)
-    # signed_txn = txn.sign(mnemonic_secret)
-    # tx_id = algod_client.send_transaction(signed_txn)
-    return 'Registration Completed.'
-        # jsonify({'success': True, 'tx_id': tx_id})
+@app.get('/extend', response_class=HTMLResponse)
+async def extend_status(request: Request, extend_name: str = Form(...),
+                          extend_duration: str = Form(...)):
+    extend_status = extend_name(extend_name, extend_duration)
+    return templates.TemplateResponse(
+        "manage.html",
+        {"request": request,
+         "extend_status": extend_status}
+    )
 
-
-@app.route('/renew', methods=['POST'])
-def renew_status():
-    renew_name = request.json.get('renew_name')
-    renew_address = request.json.get('renew_address')
-    renew_duration = request.json.get('renew_duration')
-    renew_status = renew_name(renew_name, renew_address, renew_duration)
-    return render_template('manage.html', renew_status=renew_status)
-
-
-def renew_name(renew_name, renew_address, renew_duration):
-    # if not renew_name or not renew_address or not renew_duration:
-    #     return jsonify({'error': 'Missing required parameter(s)'}), 400
-    #
-    # program = Seq([
-    #     App.localPut(Int(0), Bytes("duration"), Int(duration)),
-    #     Return(Int(1))
-    # ])
-    # teal_code = program.teal()
-    # compiled = algod_client.compile(teal_code)
-    # txn = futuretxn.Transaction(
-    #     sender=account_address,
-    #     fee=algod_client.suggested_params()['fee'],
-    #     first=algod_client.suggested_params()['last'],
-    #     last=algod_client.suggested_params()['last'] + 1000,
-    #     gh=algod_client.suggested_params()['genesis_hash'],
-    #     **compiled)
-    # signed_txn = txn.sign(mnemonic_secret)
-    # tx_id = algod_client.send_transaction(signed_txn)
-    return 'Renew Completed.'
-        # jsonify({'success': True, 'tx_id': tx_id})
-
-
-@app.route('/update', methods=['POST'])
-def update_status():
-    update_old_name = request.json.get('update_old_name')
-    update_new_name = request.json.get('update_new_name')
-    update_address = request.json.get('update_address')
-    update_duration = request.json.get('renew_duration')
-    update_status = update_name(update_old_name, update_new_name,
-                                update_address, update_duration)
-    return render_template('manage.html', update_status=update_status)
-
-
-def update_name(update_old_name, update_new_name, update_address, update_duration):
-    if not update_old_name or not update_new_name or not update_address or not update_duration:
-        return jsonify({'error': 'Missing required parameter(s)'}), 400
-    # if not encoding.is_valid_address(update_address):
-    #     return jsonify({'error': 'Invalid Algorand address'}),
-    #
-    # program = Seq([
-    #     App.localPut(Int(0), Bytes("address"), Bytes(address)),
-    #     Return(Int(1))
-    # ])
-    # teal_code = program.teal()
-    # compiled = algod_client.compile(teal_code)
-    # txn = futuretxn.Transaction(
-    #     sender=account_address,
-    #     fee=algod_client.suggested_params()['fee'],
-    #     first=algod_client.suggested_params()['last'],
-    #     last=algod_client.suggested_params()['last'] + 1000,
-    #     gh=algod_client.suggested_params()['genesis_hash'],
-    #     **compiled)
-    # signed_txn = txn.sign(mnemonic_secret)
-    # tx_id = algod_client.send_transaction(signed_txn)
-    return 'Update Completes.'
-        # jsonify({'success': True, 'tx_id': tx_id})
-
-
-if name == 'main':
-    app.run(debug=True)
+def extend_name(extend_name, extend_duration):
+    return "Success"
 
 
 
+@app.get('/renew', response_class=HTMLResponse)
+async def renew_status(request: Request, renew_name: str = Form(...),
+                          renew_duration: str = Form(...)):
+    renew_status = renew_name(renew_name, renew_duration)
+    return templates.TemplateResponse(
+        "manage.html",
+        {"request": request,
+         "renew_status": renew_status}
+    )
+
+def renew_name(renew_name, renew_duration):
+    return "Success"
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,61 +1,32 @@
 $(window).on('load', function () {
-	
+
 	'use strict';
-	
-	/* Preloader */	
+
+	/* Preloader */
     var $preloader = $('#page-preloader'),
         $spinner   = $preloader.find('.spinner');
     $spinner.fadeOut();
     $preloader.delay(350).fadeOut('slow');
-	
-	/* Portfolio Index */
-	var PORTFOLIO = (function($) {
-	var $grid = $('#grid'),	$filterOptions = $('.portfolio-menu'),
-		setupFilters = function() {
-			var $btns = $filterOptions.children();
-				$btns.on('click', function() {
-				var $this = $(this),
-				isActive = $this.hasClass( 'active' ),
-				group = isActive ? 'all' : $this.data('group');
-					if ( !isActive ) {
-						$('.portfolio-menu .active').removeClass('active');
-					}
-			$this.toggleClass('active');
-			$grid.shuffle( 'shuffle', group );
-			});
-			$btns = null;
-		},
-				init = function() {
-					setupFilters();
-					$grid.shuffle({
-						itemSelector: '.col-md-3, .col-md-4',
-					});
-				};
-		return {init: init};
-	}(jQuery));
-
-	PORTFOLIO.init();
-});
 
 $(document).ready(function() {
-	
+
 	'use strict';
-	
+
 	/* Animations Home Page*/
 	function onScrollInit( items, trigger ) {
 	    items.each( function() {
 	    var osElement = $(this),
 	    osAnimationClass = osElement.attr('data-animation'),
 	    osAnimationDelay = osElement.attr('data-animation-delay');
-		          
+
 	    osElement.css({
 	    '-webkit-animation-delay':  osAnimationDelay,
 	    '-moz-animation-delay':     osAnimationDelay,
 	    'animation-delay':          osAnimationDelay
 	    });
-			
+
 	    var osTrigger = ( trigger ) ? trigger : osElement;
-	           
+
 	    osTrigger.waypoint(function() {
 	    osElement.addClass('animated').addClass(osAnimationClass);
 	    },{
@@ -68,3 +39,34 @@ $(document).ready(function() {
 	    onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
 
 });
+
+// const form = document.getElementById("manage-form");
+// const select = document.getElementById("register_select");
+//
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//
+//   let url;
+//   let formData = new FormData(form);
+//
+//   switch (select.value) {
+//     case "register":
+//       url = "/register";
+//       break;
+//     case "extend":
+//       url = "/extend";
+//       break;
+//     case "renew":
+//       url = "/renew";
+//       break;
+//   }
+//
+//   fetch(url, {
+//     method: "GET",
+//     body: formData,
+//   })
+//     .then((response) => response.text())
+//     .then((text) => {
+//       document.body.innerHTML = text;
+//     });
+// });
