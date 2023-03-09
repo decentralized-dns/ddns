90b3874fe385b8604ee4ed1c74d2997eb917ed4b
Rui Chen <rui@chenrui.dev>
2023-03-09 00:41:55 -0500

    chore: add Brewfile for local setup (`brew bundle`)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Brewfile
@@ -0,0 +1,3 @@
+brew 'asdf'
+brew 'poetry'
+brew 'flyctl'
--- dev/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-brew install asdf poetry
-# for deployment
-brew install flyctl
-
-asdf plugin-add python
-asdf install python 3.11.2
-asdf global python 3.11.2
